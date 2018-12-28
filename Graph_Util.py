6a4120dcd24ea8bda4ca8934893f0004264ec04c
Sebastian Dixon <smedixon@gmail.com>
2018-12-28 18:07:34 +0000

    RAM graphing

--- Graph_Util.py
+++ Graph_Util.py
@@ -4,11 +4,12 @@ import pyqtgraph as pg
 import pyqtgraph.exporters
 import numpy as np
 
-
 cpu_y = []
 gpu_y = [0,1,2,3,4,5,6,7,8,9]
-#filler data as gpu utilisation function not yet made
-time_x = []
+time_x = []#
+
+ram_y = []
+ram_time_x = []
 
 def util_graphs():
     plt = pg.plot()
@@ -21,8 +22,18 @@ def util_graphs():
     line_1 = plt.plot(time_x, cpu_y, pen='b', symbol='x', symbolPen='b', symbolBrush=0.2, name='cpu')
     line_2 = plt.plot(time_x, gpu_y, pen='r', symbol='o', symbolPen='r', symbolBrush=0.2, name='gpu')
 
+def ram_util_graph():
+    plt = pg.plot()
+    plt.addLegend()
+    plt.showGrid(x=True,y=True)
+    plt.setLabel('left', 'Utilisation', units='%')
+    plt.setLabel('bottom', 'Time', units='s')
+    plt.setWindowTitle('RAM Utilisation %')
+    plt.plot(ram_time_x, ram_y, pen='b', symbol='x', symbolPen='b', symbolBrush=0.2, name='ram')
 
 if __name__ == '__main__':
     import sys
     if sys.flags.interactive != 1 or not hasattr(pg.QtCore, 'PYQT_VERSION'):
         pg.QtGui.QApplication.exec_()
+
+
